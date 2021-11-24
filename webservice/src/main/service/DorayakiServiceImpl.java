0b4d9c4d8e033c58a1c282ff649283a591645c87
Farhan <ahandnr@gmail.com>
2021-11-24 17:24:00 +0700

    template treshold

--- webservice/src/main/service/DorayakiServiceImpl.java
+++ webservice/src/main/service/DorayakiServiceImpl.java
@@ -7,23 +7,29 @@ import java.sql.*;
 @WebService(endpointInterface = "main.services.DorayakiService")
 public class DorayakiServiceImpl {
 
-    public static void main(String[] args) {
+    public static String RequestLimiter(String ip, String end){
 
         try {
             DBHandler handler = new DBHandler();
             Connection conn = handler.getConnection();
             Statement statement = conn.createStatement();
-            String query = "select * from bahan_baku";
+            String query = String.format("select count(*) from log_request where ip = %s and endpoint = %s and timestamp between now() - interval 1 minute and now()",ip,end);
+//            String query = String.format("select * from log_request where ip = %s",ip);
             ResultSet result = statement.executeQuery(query);
 
             while(result.next()) {
-                System.out.println(result.getString("namaBahan"));
+                System.out.println(result.getString("count(*)"));
             }
-//            return "Berhasil";
+            return "Berhasil";
 
         } catch (Exception e) {
             e.printStackTrace();
-//            return "gagal";
+            return "gagal";
         }
     }
+
+    public static void main(String[] args) {
+        String a = RequestLimiter("121212","2");
+        System.out.println(a);
+       }
 }
