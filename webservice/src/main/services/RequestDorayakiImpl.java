33ca54aa13c35590ffc365cd4dadd622c7549180
Farhan <ahandnr@gmail.com>
2021-11-25 22:51:55 +0700

    rest request

--- webservice/src/main/services/RequestDorayakiImpl.java
+++ webservice/src/main/services/RequestDorayakiImpl.java
@@ -1,12 +1,78 @@
 package main.services;
 
 import javax.jws.WebService;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.sql.Timestamp;
 
 @WebService(endpointInterface = "main.services.RequestDorayaki")
 public class RequestDorayakiImpl implements RequestDorayaki {
 
     @Override
     public String RequestDorayakiPabrik(String ip, int id, int jumlah) {
-        return "belom req";
+
+        RateLimiter ratelimiter = new RateLimiter();
+        int status = ratelimiter.RateLimiter(ip,"list", new Timestamp(System.currentTimeMillis()));
+        if (status == 1) {
+            sendReq("disini string json");
+            return "Request Berhasil";
+
+        }
+        else if (status == 0) {
+            return "Anda melakukan terlalu banyak request, harap tunggu...";
+        }
+        else {
+            return "Terjadi kesalahan, silahkan coba lain kali.";
+        }
+
     }
+
+    public void sendReq(String input) {
+
+        try {
+
+            URL url = new URL("http://localhost:8080/RESTfulExample/json/product/post");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
+
+            OutputStream os = conn.getOutputStream();
+            os.write(input.getBytes());
+            os.flush();
+
+            if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            String output;
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+
+            e.printStackTrace();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+
 }
